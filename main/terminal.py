fb215bf6f1fc61968683b15babb192c1db7a2d23
Brito <jpbrito@utad.pt>
2019-03-18 18:40:18 +0000

    recolha passou a classes

---
+++ main/terminal.py
@@ -0,0 +1,75 @@
+from time import sleep
+from main.simple import MQTTClient
+from machine import Pin, I2C, ADC
+import main.bmp280 as bmp280
+from dht import DHT22
+import main.adafruit_sgp30 as adafruit_sgp30
+import main.mq135 as mq135
+import network
+import json
+
+class Terminal:
+    def __init__(self,node_name,node_ip,node_gateway,node_wifi_ssid,node_wifi_password):
+        self.node_name = node_name
+        self.node_ip = node_ip
+        self.node_gateway = node_gateway
+        self.node_wifi_ssid = node_wifi_ssid
+        self.node_wifi_password = node_wifi_password
+        self.client = MQTTClient(self.node_name, self.node_gateway)
+        self.sensordht = DHT22(Pin(15, Pin.IN, Pin.PULL_UP))
+        self.i2c = I2C(scl=Pin(22), sda=Pin(21))
+        self.i2csgp30 = I2C(scl=Pin(17), sda=Pin(16))
+        self.bmp = bmp280.BMP280(self.i2c)
+        self.sgp30 = adafruit_sgp30.Adafruit_SGP30(self.i2csgp30)
+        self.sgp30.iaq_init()
+        self.sgp30.set_iaq_baseline(0x8973, 0x8aae)
+        self.mq135 = mq135.MQ135(Pin(36))
+        self.data = {}
+        self.data["node"] = self.node_name
+
+    #publish json via mqtt to topic
+    def sendframe(self):
+        self.client.publish("/terminal", json.dumps(self.data))
+
+   # 1) read dht22 temp and humidity
+    def readDHT22(self):
+        self.sensordht.measure()
+        if isinstance(self.sensordht.temperature(), float) and isinstance(self.sensordht.humidity(), float):
+            self.data["dht22_temp"] = self.sensordht.temperature()
+            self.data["dht22_hum"] = self.sensordht.humidity()
+        else:
+            self.data["dht22_temp"] = -1
+            self.data["dht22_hum"] = -1
+
+    # 2) read mq135 CO2 PPM and other values
+    def readMQ135(self):
+        self.data["rzero"] = self.mq135.get_rzero()
+        self.data["corrected_rzero"] = self.mq135.get_corrected_rzero(22,55)
+        self.data["ppm"] = self.mq135.get_ppm()
+        if self.data["dht22_temp"] == -1:
+            self.data["corrected_ppm"] = self.mq135.get_corrected_ppm(22,55) # error reading dht we assume default values for temp and hum
+        else:
+            self.data["corrected_ppm"] = self.mq135.get_corrected_ppm(self.data["dht22_temp"],self.data["dht22_hum"])
+        if self.data["ppm"] >= 4000 or self.data["corrected_ppm"] >= 4000:
+            self.data["ppm"] = -1
+            self.data["corrected_ppm"] = -1
+        self.data["resistance"] = self.mq135.get_resistance()
+
+
+    # 3) read bmp280 temp and pressure
+    def readBMP280(self):
+        self.data["bmp280_temp"] = self.bmp.temperature
+        self.data["bmp280_pressure"] = self.bmp.pressure
+
+    # 4) read sgp30 co2 and tvoc
+    def readSGP30(self):
+        self.data["sgp30_co2"] = self.sgp30.co2eq
+        self.data["sgp30_tvoc"] = self.sgp30.tvoc
+
+    # 5) read sensor data
+    def read(self):
+        self.readDHT22()
+        self.readMQ135()
+        self.readBMP280()
+        self.readSGP30()
+        self.sendframe()
